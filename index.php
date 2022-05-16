8978e7e52807ce125bf7e4513637ce5ad249f238
thiagoribeiro2003 <thiagoribeiro20003@gmail.com>
2022-05-16 08:40:43 -0300

    index geral e lista de fabricantes

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>CRUD Vendas</title>
+</head>
+<body>
+    <div class="container">
+        <h1>CRUD - Sistema Vendas</h1>
+        <hr>
+        <p>Operações de <b>inserção</b>, <b>leitura</b>, <b>atualização</b> e <b>exclusão</b> de dados.</p>
+
+        <ul>
+            <li><a href="fabricantes/listar.php">Fabricantes</a></li>
+            <li><a href="">Produtos</a></li>
+        </ul>
+    </div>
+</body>
+</html>
