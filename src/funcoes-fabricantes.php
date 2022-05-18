c85c02576e02657cbc60a349f9ad386e1d8028b5
thiagoribeiro2003 <thiagoribeiro20003@gmail.com>
2022-05-18 08:55:55 -0300

    .

--- src/funcoes-fabricantes.php
+++ src/funcoes-fabricantes.php
@@ -15,7 +15,17 @@ function lerFabricantes(PDO $conexao):array {
 }
 
 // Inserir um fabricante
-function inserirFabricante(PDO $conexao, string $nome) {
-    
+function inserirFabricante(PDO $conexao, string $nome): void /*  void significa que não tem retorno*/ {
+    /* :qualquer_coisa: named parameters */
+    $sql = "INSERT INTO fabricantes(nome) VALUES('$nome')";
+    try {
+     $consulta = $conexao-prepare($sql);
+
+     /* bindParam('nome do parametro', $variavel_com_valor, constante de verificação) */
+     $consulta->bindParam(':nome', $nome, PDO::PARAM_STR);
+     $consulta->execute();
+    } catch (Exception $erro) {
+        die("Erro: ".$erro->getMessage());
+    }
 }
 ?>
