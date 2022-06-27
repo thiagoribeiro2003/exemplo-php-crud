1901c236b903c3b969b93413deeb4d09fd434065
thiagoribeiro2003 <thiagoribeiro20003@gmail.com>
2022-06-27 08:31:18 -0300

    Update index.php

--- index.php
+++ index.php
@@ -4,13 +4,14 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>CRUD Vendas</title>
+    <title>CRUD - Sistema Vendas (Versão POO)</title>
 </head>
 <body>
     <div class="container">
         <h1>CRUD - Sistema Vendas</h1>
         <hr>
-        <p>Operações de <b>inserção</b>, <b>leitura</b>, <b>atualização</b> e <b>exclusão</b> de dados.</p>
+
+        <p><i>Versão usando Namespaces, Composer, Autoload, classes, métodos, propriedades</i></p>
 
         <ul>
             <li><a href="fabricantes/listar.php">Fabricantes</a></li>
