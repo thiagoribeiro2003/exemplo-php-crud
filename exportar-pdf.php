3418b380b3d0fd45e4d2779230f84f7136b3429b
thiagoribeiro2003 <thiagoribeiro20003@gmail.com>
2022-07-18 10:38:02 -0300

    Update exportar-pdf.php

--- exportar-pdf.php
+++ exportar-pdf.php
@@ -1,4 +1,14 @@
 <?php
 // exportar-pdf.php
+use CrudDiversos\Utilitarios;
+
+require_once "vendor/autoload.php";
+
+session_start(); // inicialização a session
+// Utilitarios::teste($_SESSION['dados']);
+
+foreach($_SESSION['dados'] as $fabricante){
+    echo $fabricante['nome'] ."<br>";
+}
 
 echo "Script de exportação em PDF";
